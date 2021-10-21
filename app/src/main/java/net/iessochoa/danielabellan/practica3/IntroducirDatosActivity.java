f8bf71807966f3a13fe2b60196c707ba437b3de9
DanielAZ <danielabezarate@gmail.com>
2021-10-21 17:26:21 +0200

    Creada la interfaz de la actividad de introducción de datos

--- app/src/main/java/net/iessochoa/danielabellan/practica3/IntroducirDatosActivity.java
+++ app/src/main/java/net/iessochoa/danielabellan/practica3/IntroducirDatosActivity.java
@@ -2,12 +2,26 @@ package net.iessochoa.danielabellan.practica3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class IntroducirDatosActivity extends AppCompatActivity {
+
+    private EditText etDato;
+    private Button btnOkDatos;
+    private Button btnCancelarDatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introducir_datos);
+
+        etDato = findViewById(R.id.etDato);
+        btnOkDatos = findViewById(R.id.btnOkDatos);
+        btnCancelarDatos = findViewById(R.id.btnCancelarDatos);
+
+        btnCancelarDatos.setOnClickListener(view -> {
+            this.finish();
+        });
     }
 }
